e35ac36f4a3ed3774d76ba1eef05aaf3d1b6a975
Ahmed Khalid <72296578+ahmedkhalid7@users.noreply.github.com>
2023-10-04 17:25:38 +0300

    added missing week7 materials

---
+++ Week7/Tasks/JDBCandHibernate/target/classes/db/DrugTablesQueries.sql
@@ -0,0 +1,25 @@
+INSERT INTO Drug (drug_id, drug_name, drug_description)
+VALUES
+  (1, 'Advil', 'A pain reliever that is used to treat headaches, fevers, and other minor aches and pains.'),
+  (2, 'Tylenol', 'A pain reliever and fever reducer.'),
+  (3, 'Ibuprofen', 'A nonsteroidal anti-inflammatory drug (NSAID) that is used to treat pain, fever, and inflammation.'),
+  (4, 'Naproxen', 'A nonsteroidal anti-inflammatory drug (NSAID) that is used to treat pain, fever, and inflammation.'),
+  (5, 'Acetaminophen', 'A pain reliever and fever reducer.'),
+  (6, 'Codeine', 'A narcotic pain reliever that is used to treat moderate to severe pain.'),
+  (7, 'Morphine', 'A narcotic pain reliever that is used to treat severe pain.'),
+  (8, 'Oxycodone', 'A narcotic pain reliever that is used to treat moderate to severe pain.'),
+  (9, 'Hydrocodone', 'A narcotic pain reliever that is used to treat moderate to severe pain.'),
+  (10, 'Diazepam', 'A benzodiazepine that is used to treat anxiety, insomnia, and seizures.'),
+  (11, 'Lorazepam', 'A benzodiazepine that is used to treat anxiety, insomnia, and seizures.'),
+  (12, 'Xanax', 'A benzodiazepine that is used to treat anxiety, insomnia, and panic attacks.'),
+  (13, 'Clonazepam', 'A benzodiazepine that is used to treat anxiety, insomnia, and seizures.'),
+  (14, 'Alprazolam', 'A benzodiazepine that is used to treat anxiety and panic attacks.'),
+  (15, 'Fluoxetine', 'An antidepressant that is used to treat depression.'),
+  (16, 'Sertraline', 'An antidepressant that is used to treat depression.'),
+  (17, 'Citalopram', 'An antidepressant that is used to treat depression.'),
+  (18, 'Escitalopram', 'An antidepressant that is used to treat depression.'),
+  (19, 'Bupropion', 'An antidepressant that is used to treat depression and seasonal affective disorder.'),
+  (20, 'Mirtazapine', 'An antidepressant that is used to treat depression and anxiety.');
+
+
+SELECT * FROM HospitalDatabase.Drug;
